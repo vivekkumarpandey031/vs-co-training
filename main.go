db52c1ef84b94e1af045be0b8a139354ff411d30
Vivek kumar pandey <85150885+vivekkumarpandey031@users.noreply.github.com>
2023-08-08 00:00:04 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	fmt.Println("Welcome to my world")
+}
+ //https://github.com/JitenPalaparthi/victoria-secrets-training-golang
+ //tricolor garbage collector in golang
+ // escape analysis
+ 
